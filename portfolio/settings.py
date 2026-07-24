2fed91440c832ad1c0f3dbf5269b13f06b9d2d1a
Muhammad Danish <danishriazali501@gmail.com>
2026-07-24 17:06:46 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'gli-MAZHJSSPMlHbS082jFmkP-MCyqv03idsCKC9WQ-dMebneVs6gY2_tMqkLJukk0E')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "False"
 
 ALLOWED_HOSTS = ['*']
 
